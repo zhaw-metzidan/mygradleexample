e3638a9607388e3135126cd347af03c79783248e
Daniel <43812723+zhaw-metzidan@users.noreply.github.com>
2019-03-07 15:02:05 +0100

    Add files via upload

---
+++ myGradleExample/src/main/java/example/helloGradle.java
@@ -0,0 +1,10 @@
+package main.java.example;
+
+public class helloGradle {
+
+	public static void main(String[] args) {
+		// https://www.mkyong.com/gradle/gradle-create-a-jar-file-with-dependencies/
+		System.out.println("hello gradle");
+	}
+
+}
